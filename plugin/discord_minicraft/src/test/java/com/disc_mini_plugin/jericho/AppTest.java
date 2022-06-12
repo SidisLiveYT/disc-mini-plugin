fd74972daa461becd8b2b0995d88659073cb7496
Sid is Live YT <sidisliveytofficial@gmail.com>
2022-06-12 19:58:19 +0530

    chore: Boiler Plate for mini-plugin

---
+++ plugin/discord_minicraft/src/test/java/com/disc_mini_plugin/jericho/AppTest.java
@@ -0,0 +1,20 @@
+package com.disc_mini_plugin.jericho;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
